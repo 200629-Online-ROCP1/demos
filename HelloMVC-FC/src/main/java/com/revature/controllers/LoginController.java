790c2c6568634211634dade0f50d2d26228fc438
gattietime <gattietime@gmail.com>
2020-07-15 17:21:38 -0400

    added models to MVC demo

--- HelloMVC-FC/src/main/java/com/revature/controllers/LoginController.java
+++ HelloMVC-FC/src/main/java/com/revature/controllers/LoginController.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.revature.models.User;
+
 public class LoginController {
 	
 	public static String login(HttpServletRequest req, HttpServletResponse res) {
@@ -14,8 +16,11 @@ public class LoginController {
 		
 		String username = req.getParameter("username");
 		String password = req.getParameter("password"); 
+		User u = new User(username,password);
 		
-		if(username.equals("derp") && password.equals("drowssap")) {
+		User u2 = new User("derp","drowssap");
+		
+		if(u.username.equals(u2.username) && u.password.equals(u2.password)) {
 			System.out.println("login worked!");
 			
 			return "/api/Home";
---
+++ HelloMVC-FC/src/main/java/com/revature/models/User.java
@@ -0,0 +1,19 @@
+package com.revature.models;
+
+public class User {
+	
+	public String username;
+	public String password;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+
+}
